99644aa65d66236d217e3933fe1d2efde7f950a4
Shreyansh <sjaiswal0902@gmail.com>
2023-11-17 19:36:17 +0530

    Displays suggestions as one enters a name.. Commit By User Shreyansh

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,20 @@ import Navbar from "./components/navbar";
 import Dashboard from "./components/userdashboard";
 import UserWidget from "./widgets/UserWidget";
 import RegistrationForm from "./components/register";
+import { useState } from "react";
+import { SearchResultsList } from "./components/SearchResultsList";
 function App() {
+  const [results, setResults] = useState([]);
+
   return (
     <div className="App" style={{backgroundColor:"#e5e8e3"}}>
-    <Navbar />
+    <Navbar setResults={setResults}/>
+    <SearchResultsList results={results}/>
     <BrowserRouter>
       <Routes>
         <Route path = "/jobpostform" element={<JobForm />}></Route>
         <Route path = "/profile" element={<Dashboard />}></Route>
-        <Route path = "/" element={<UserWidget userId={_id} picturePath={picturePath}/>}></Route>
+        <Route path = "/" element={/*<UserWidget userId={_id} picturePath={picturePath}/>*/ <RegistrationForm />}></Route>
       </Routes>
     </BrowserRouter>
     </div>
